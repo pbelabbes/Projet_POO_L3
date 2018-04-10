63b20264b4c98c595353de9327131f8bf0b7a658
Pierre Belabbes <pbelabbes@gmail.com>
2018-04-10 16:08:36 +0200

    add TestBasic file

--- src/kernel/SGBD.java
+++ src/kernel/SGBD.java
@@ -1,13 +1,17 @@
 package kernel;
 
+import java.util.ArrayList;
+
 public class SGBD {
+	
+	private static ArrayList<DB> DBs = new ArrayList<DB>();
 
 	public static DB getDbByName(String db_name) {
 		return null;
 	}
 	
 	public static void addDB(DB new_database) {
-		
+		DBs.add(new_database);
 	}
 	
 }
