7edd63becff79f4cd8b87be934d0b6d16ce3d5e0
Pierre Belabbes <pbelabbes@gmail.com>
2018-04-12 11:04:31 +0200

    Javadoc DB class

--- src/kernel/DB.java
+++ src/kernel/DB.java
@@ -6,32 +6,62 @@ import kernel.relation.Relation;
 
 public class DB {
 
+	/**
+	 * List of tables include in the database
+	 */
 	private ArrayList<Relation> relations;
 
+	/**
+	 * name of the database
+	 */
 	private String name;
 
+	/**
+	 * 
+	 * @param name , name of the database
+	 */
 	public DB(String name ) {
 		this.name = name;
 		this.relations = new ArrayList<Relation>();
 	}
 
+	/**
+	 * 
+	 * @param name , name of the database 
+	 * @param relations 
+	 */
 	public DB(String name, ArrayList<Relation> relations) {
 		this.name = name;
 		this.relations = relations;
 	}
 
+	/**
+	 * 
+	 * @param rel table that will be added in the database
+	 */
 	public void addRelation(Relation rel) {
 		this.relations.add(rel);
 	}
 
+	/**
+	 * 
+	 * @param rel talbe that will be deleted in the database
+	 */
 	public void deleteRelation(Relation rel) {
 		this.relations.remove(rel);
 	}
 
+	/**
+	 * 
+	 * @return the name of the database
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * show all the tables in the database
+	 */
 	public void showTables() {
 		if(this.relations.isEmpty()) System.out.println("No table in the database");
 		for(Relation table : this.relations) {
@@ -39,7 +69,11 @@ public class DB {
 		}
 	}
 
-
+	/**
+	 * 
+	 * @param table_name 
+	 * @return
+	 */
 	public Relation getTableByName(String table_name) {
 		
 		Relation res = null;
